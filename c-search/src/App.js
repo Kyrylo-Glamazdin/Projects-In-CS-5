b0eeda26715ff1045c9f0ef1e279e8bd63384147
Kyrylo Glamazdin <kyrylo.glamazdin36@myhunter.cuny.edu>
2020-07-02 13:59:18 -0400

    initialized city app

---
+++ c-search/src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      enteredCity: ""
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
